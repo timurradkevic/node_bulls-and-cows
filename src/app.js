0db118ade3ad26fdfa49ae150800c44a11a0302f
Tymur <timurradkevic84@mail.com>
2026-05-18 09:46:04 +0300

    add task solution

--- src/app.js
+++ src/app.js
@@ -1,3 +1,31 @@
+/* eslint-disable no-console */
 'use strict';
 
-// Write your code here
+import readline from 'node:readline';
+import { getBullsAndCows } from './modules/getBullsAndCows.js';
+import { generateRandomNumber } from './modules/generateRandomNumber.js';
+import { checkIsValidUserInput } from './modules/checkIsValidUserInput.js';
+
+const numberToGuess = generateRandomNumber();
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function ask() {
+  rl.question('What is your guess? ', (userInput) => {
+    if (!checkIsValidUserInput(userInput)) {
+      console.log('Enter valid number');
+      ask();
+    } else if (getBullsAndCows(userInput, numberToGuess).bulls === 4) {
+      console.log('You win!');
+      rl.close();
+    } else {
+      console.log(getBullsAndCows(userInput, numberToGuess));
+      ask();
+    }
+  });
+}
+
+ask();
